346f26a1b4be7cfcd4dd5b9e09c2b771aa3ee201
prjemian <prjemian@gmail.com>
2018-03-16 15:13:19 -0500

    diagnostics

--- profile_bluesky/startup/00-startup.py
+++ profile_bluesky/startup/00-startup.py
@@ -38,3 +38,12 @@ def append_wa_motor_list(*motorlist):
 # import logging
 # ophyd.logger.setLevel(logging.DEBUG)
 # logging.basicConfig(level=logging.DEBUG)
+
+# if needed for the EPICS areaDetector SimDetector (12M + 100)
+# os.environ["EPICS_CA_MAX_ARRAY_BYTES"] = "12000100"
+
+
+# diagnostics
+from bluesky.utils import ts_msg_hook
+#RE.msg_hook = ts_msg_hook
+from bluesky.simulators import summarize_plan
