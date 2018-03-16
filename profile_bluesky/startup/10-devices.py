19457af057948f5608b78032afdbbce4821d5833
prjemian <prjemian@gmail.com>
2018-03-16 15:13:48 -0500

    use standard devices

--- profile_bluesky/startup/10-devices.py
+++ profile_bluesky/startup/10-devices.py
@@ -5,26 +5,31 @@ print(__file__)
 import time
 from ophyd import Component, Device, DeviceStatus
 from ophyd import EpicsMotor, EpicsScaler
+from ophyd.scaler import ScalerCH
 from ophyd import EpicsSignal, EpicsSignalRO, EpicsSignalWithRBV
 from ophyd import PVPositioner, PVPositionerPC
 from ophyd import AreaDetector, PcoDetectorCam
 from ophyd import SingleTrigger, ImagePlugin, HDF5Plugin
 from ophyd.areadetector.filestore_mixins import FileStoreHDF5IterativeWrite
-from APS_BlueSky_tools.devices import userCalcsDevice
-# from APS_BlueSky_tools.synApps_ophyd import *
+from APS_BlueSky_tools.devices import *
+#from APS_BlueSky_tools.synApps_ophyd import *
+from APS_BlueSky_tools import *
+from APS_BlueSky_tools.plans import *
 
 
-class MotorDialValuesDevice(Device):
-    value = Component(EpicsSignalRO, ".DRBV")
-    setpoint = Component(EpicsSignal, ".DVAL")
+class TunableEpicsMotor(EpicsMotor, AxisTunerMixin):
+	"""
+	Example::
+	
+        def a2r_pretune_hook():
+            # set the counting time for *this* tune
+            yield from bps.abs_set(scaler.preset_time, 0.2)
+            
+        a2r = TunableEpicsMotor("xxx:m1", name="a2r")
+        a2r.tuner = TuneAxis([scaler], a2r, signal_name=scaler.channels.chan2.name)
+        a2r.tuner.width = 0.02
+        a2r.tuner.num = 21
+        a2r.pre_tune_method = a2r_pretune_hook
+        RE(a2r.tune())
 
-
-class MyEpicsMotorWithDial(EpicsMotor):
-    dial = Component(MotorDialValuesDevice, "")
-
-
-class ServoRotationStage(EpicsMotor):
-    """extend basic motor support to enable/disable the servo loop controls"""
-    
-    # values: "Enable" or "Disable"
-    servo = Component(EpicsSignal, ".CNEN", string=True)
+	"""
