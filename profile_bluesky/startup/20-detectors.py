4678a825daa807804f5ec5bc29b5546f36f62b55
prjemian <prjemian@gmail.com>
2018-03-16 15:14:31 -0500

    have scalers find out the channels named in EPICS

--- profile_bluesky/startup/20-detectors.py
+++ profile_bluesky/startup/20-detectors.py
@@ -4,10 +4,24 @@ print(__file__)
 
 from APS_BlueSky_tools.examples import SynPseudoVoigt
 
+
+def use_EPICS_scaler_channels(scaler):
+    """
+    configure scaler for only the channels with names assigned in EPICS 
+    """
+    read_attrs = []
+    for ch in scaler.channels.component_names:
+        _nam = epics.caget("{}.NM{}".format(scaler.prefix, int(ch[4:])))
+        if len(_nam.strip()) > 0:
+            read_attrs.append(ch)
+    scaler.channels.read_attrs = read_attrs
+
+
 noisy = EpicsSignalRO('gov:userCalc1', name='noisy')
-scaler = EpicsScaler('gov:scaler1', name='scaler')
-# but only read a few of the many channels
-scaler.channels.read_attrs = "chan1 chan2 chan3 chan6".split()
+#scaler = EpicsScaler('gov:scaler1', name='scaler')
+scaler = ScalerCH('gov:scaler1', name='scaler')
+use_EPICS_scaler_channels(scaler)
+
 
 synthetic_pseudovoigt = SynPseudoVoigt(
     'synthetic_pseudovoigt', m1, 'm1', 
