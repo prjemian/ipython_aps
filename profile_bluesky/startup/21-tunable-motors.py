d18c660aa6b6c877871c117a81820529ad89ebb6
prjemian <prjemian@gmail.com>
2018-03-21 15:28:46 -0500

    no special name for the tuning signal in this case

--- profile_bluesky/startup/21-tunable-motors.py
+++ profile_bluesky/startup/21-tunable-motors.py
@@ -2,9 +2,6 @@ print(__file__)
 
 """tunable motors"""
 
-try:
-    m1.tuner = TuneAxis([synthetic_pseudovoigt], m1, signal_name=scaler.channels.chan2.name)
-except AttributeError:
-    m1.tuner = TuneAxis([synthetic_pseudovoigt], m1, signal_name=scaler.channels.chan02.name)
+m1.tuner = TuneAxis([synthetic_pseudovoigt], m1)
 m1.tuner.width = 0.02
 m1.tuner.num = 21
