37a29a02ef746df6e40ca53170e71baee2d5a4b3
prjemian <prjemian@gmail.com>
2018-05-31 16:29:17 -0500

    turn off for now

--- profile_bluesky/startup/80-callbacks.py
+++ profile_bluesky/startup/80-callbacks.py
@@ -6,10 +6,10 @@ import APS_BlueSky_tools.callbacks
 import APS_BlueSky_tools.filewriters
 
 
-doc_collector = APS_BlueSky_tools.callbacks.DocumentCollectorCallback()
-callback_db['doc_collector'] = RE.subscribe(doc_collector.receiver)
+#doc_collector = APS_BlueSky_tools.callbacks.DocumentCollectorCallback()
+#callback_db['doc_collector'] = RE.subscribe(doc_collector.receiver)
 
-specwriter = APS_BlueSky_tools.filewriters.SpecWriterCallback()
-specwriter.newfile(os.path.join("/tmp", specwriter.spec_filename))
-callback_db['specwriter'] = RE.subscribe(specwriter.receiver)
-print("SPEC data file:", specwriter.spec_filename)
+#specwriter = APS_BlueSky_tools.filewriters.SpecWriterCallback()
+#specwriter.newfile(os.path.join("/tmp", specwriter.spec_filename))
+#callback_db['specwriter'] = RE.subscribe(specwriter.receiver)
+#print("SPEC data file:", specwriter.spec_filename)
