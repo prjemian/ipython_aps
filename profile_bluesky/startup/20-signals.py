a1e2278220d61ba834008bfb9643b9264d8bb10a
prjemian <prjemian@gmail.com>
2018-06-01 09:28:24 -0500

    example: monitor secondary data during scans

--- profile_bluesky/startup/20-signals.py
+++ profile_bluesky/startup/20-signals.py
@@ -2,6 +2,14 @@ print(__file__)
 
 """other signals"""
 
+aps_sr_current = EpicsSignalRO("S:SRcurrentAI", name="aps_sr_current")
+
+# always record this as secondary stream during scans
+# name: aps_sr_current_monitor
+# db[-1].table("aps_sr_current_monitor")
+sd.monitors.append(aps_sr_current)
+
+
 scans = sscanDevice("gov:", name="scans")
 calcs = userCalcsDevice("gov:", name="calcs")
 calcs.enable.put("Enable")
