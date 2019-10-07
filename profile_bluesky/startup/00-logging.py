209ba89fd0797ad6a05ad31a32742889a56f01c8
Peter Jemian <jemian@otz.xray.aps.anl.gov>
2019-10-07 16:23:20 -0500

    refactor to use stdlogpj

--- profile_bluesky/startup/00-logging.py
+++ profile_bluesky/startup/00-logging.py
@@ -4,8 +4,8 @@ print(__file__)
 configure logging
 """
 
-LOGGER_FILE = ".ipython_logger.log"
-CONSOLE_IO_FILE = ".ipython_console.log"
+os.makedirs(".logs", exist_ok=True)
+CONSOLE_IO_FILE = ".logs/.ipython_console.log"
 
 # start logging console to file
 # https://ipython.org/ipython-doc/3/interactive/magics.html#magic-logstart
@@ -14,38 +14,8 @@ _ipython = get_ipython()
 # %logstart -o -t .ipython_console.log "rotate"
 _ipython.magic(f"logstart -o -t {CONSOLE_IO_FILE} rotate")
 
-
-# Uncomment the following lines to turn on 
-# verbose messages for debugging.
-import logging
-#ophyd.logger.setLevel(logging.DEBUG)
-#logging.basicConfig(level=logging.DEBUG)
-
-logger = logging.getLogger(os.path.split(__file__)[-1])
-
-stderr_log_handler = logging.StreamHandler()
-logger.addHandler(stderr_log_handler)
-
-# nice output format
-# https://docs.python.org/3/library/logging.html#logrecord-attributes
-stderr_log_format = "%(levelname)-.1s"		# only first letter
-stderr_log_format += " %(asctime)s"
-stderr_log_format += " - "
-stderr_log_format += "%(message)s"
-stderr_log_handler.setFormatter(logging.Formatter(stderr_log_format))
-stderr_log_handler.formatter.default_msec_format = "%s.%03d"
-
-file_log_handler = logging.FileHandler(LOGGER_FILE)
-logger.addHandler(file_log_handler)
-file_log_format = "%(asctime)s"
-file_log_format += ",%(levelname)s"
-file_log_format += ",%(name)s"
-file_log_format += ",%(module)s"
-file_log_format += ",%(lineno)d"
-file_log_format += " - "
-file_log_format += "%(message)s"
-file_log_handler.setFormatter(logging.Formatter(file_log_format))
-file_log_handler.formatter.default_msec_format = "%s.%03d"
+import stdlogpj
+logger = stdlogpj.standard_logging_setup("ipython_logger")
 
 logger.warning('logging started')
 
